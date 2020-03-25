1f6ac64fee6934463bcaa322f549c2a0aa0adac4
Somil Bichpuriya <57514538+sbichpuriya11@users.noreply.github.com>
2020-03-25 23:08:11 +0530

    Add files via upload

--- Inherited/index.php
+++ Inherited/index.php
@@ -1,24 +1,16 @@
 <?php
-$db['db_host']="localhost";
-$db['db_user']="root";
-$db['db_pass']="";
-$db['db_name']="countries";
-foreach($db as $key => $value){
-define(strtoupper($key), $value);
-}
-$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+include 'includes/db.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
     <link rel="shortcut icon" href="raining.png" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
-
-
+    <script src="js/main.js"></script>
     <!--Bootstrap-->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
@@ -28,245 +20,235 @@ $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <!--Fontawesome-->
     <script src="https://kit.fontawesome.com/ac8add5dc3.js" crossorigin="anonymous"></script>
-
     <!--Custom Bootstrap for celcius and farhenote toggling-->
     
-
     
-</head>
-
-<body class="bg">
+  </head>
+  <body class="bg">
     <div class="container-fluid background">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-transparent py-2">
-                <a class="navbar-brand" href="#"><i class="fas fa-cloud-sun-rain py-2" style="font-size: 50px;"></i>
-                    <div class="row" style="letter-spacing:1px;">Rainfall Prediction</div>
-                </a>
-                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <div style="border:1px solid white; color:white!important;outline:none;border-radius: 2px; padding:3px;"><span class="navbar-toggler-icon"></span>Menu</div>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0 p-4">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search By City or Postcode"
-                            aria-label="Search" id="search-field">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search <i class="fas fa-search"></i></button>
-                    </form>
-
-
-
-                    <a href="" data-toggle="modal" type="button" class="btn btn-danger" data-placement=" bottom" data-target="#mymodal"><strong>IN <i class="fas fa-globe-asia"></i> |  C &deg; <i class="fas fa-caret-down"></i></strong></a> 
-                    <div class="modal fade p-3" id="mymodal" role="dialog">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close text-left" data-dismiss="modal">&times;</button>
-                                </div>
-                                <div class="modal-body">
-                                    
-
-
-                                    <div class="onoffswitch4">
-                                        <input type="checkbox" name="onoffswitch4" class="onoffswitch4-checkbox" id="myonoffswitch4" checked>
-                                        <label class="onoffswitch4-label" for="myonoffswitch4">
-                                            <span class="onoffswitch4-inner"></span>
-                                            <span class="onoffswitch4-switch"></span>
-                                        </label>
-                                    </div>
-                                    
-                                   
-
-
-
-
-
-
-                                   
-
-<div class="accordion py-3" id="accordionExample">
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-          Asia
-        </button>
-      </h5>
-    </div>
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-      data-parent="#accordionExample">
-      <div class="card-body">
-          <!--List goes here-->
-      </div>
-    </div>
-  </div>
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left collapsed text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Africa
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-       <!--List goes here-->
-      </div>
-    </div>
-  </div>
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left text-decoration-none collapsed" type="button" data-toggle="collapse"
-          data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Europe
-        </button>
-      </h5>
-    </div>
-
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-       <!--List goes here-->
-      </div>
-    </div>
-  </div>
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left text-decoration-none collapsed" type="button" data-toggle="collapse"
-          data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          North America
-        </button>
-      </h5>
-    </div>
-
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-       <!--List goes here-->
-      </div>
-    </div>
-  </div>
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left text-decoration-none collapsed" type="button" data-toggle="collapse"
-          data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-         South America
-        </button>
-      </h5>
-    </div>
-
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-       <!--List goes here-->
-      </div>
-    </div>
-  </div>
-  <div class="card z-depth-0 bordered">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link w-100 text-left text-decoration-none collapsed" type="button" data-toggle="collapse"
-          data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Oceania
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-          <!--List goes here-->
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-                                    
-
-                                </div>
-                                
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                
-
-
-
-
-
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-transparent py-2">
+          <a class="navbar-brand" href="#"><i class="fas fa-cloud-sun-rain py-2" style="font-size: 50px;"></i>
+            <div class="row" style="letter-spacing:1px;">Rainfall Prediction</div>
+          </a>
+          <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <div style="border:1px solid white; color:white!important;outline:none;border-radius: 2px; padding:3px;"><span class="navbar-toggler-icon"></span>Menu</div>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Disabled</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0 p-4">
+              <input class="form-control mr-sm-2" type="search" style="font-size:12px" placeholder="Search City or Postcode"
+              aria-label="Search" id="search-field">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search <i class="fas fa-search"></i></button>
+            </form>
+            <a href="" data-toggle="modal" type="button" class="btn btn-danger" data-placement="bottom" data-target="#mymodal">
+            	IN <i class="fas fa-globe-asia"></i> |  C &deg; <i class="fas fa-caret-down"></i>
+            </a>
+            <div class="modal fade p-3" id="mymodal" role="dialog">
+              <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+                  <div class="modal-body">
                     
+                  
+
+
+
+                  <div class="section text-left over-hide z-bigger">
+                    <div class="col-12 pb-2">
+                      <input class="checkbox-tools" type="radio" name="tools" id="tool-1" checked>
+                      <label class="for-checkbox-tools" for="tool-1">&#176;C</label>
+                      <input class="checkbox-tools" type="radio" name="tools" id="tool-2">
+                      <label class="for-checkbox-tools" for="tool-2">&#176;F
+                      </label>
+                    </div>
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+                    <div class="accordion pt-5" id="accordionExample">
+                      <div class="card">
+                        <div class="card-header" id="headingOne">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-left text-decoration-none text-info" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          Asia
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                          
+                          <div class="card-body text-dark">
+                            <?php
+                              include 'includes/asia.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingTwo">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-left text-decoration-none text-info" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          Africa
+
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                          <div class="card-body text-dark">
+
+                          <?php
+                              include 'includes/africa.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingThree">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-decoration-none text-info text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          Europe
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                          <div class="card-body text-dark">
+                          <?php
+                              include 'includes/europe.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingThree">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-decoration-none text-info text-left" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree">
+                          South America
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                          <div class="card-body text-dark">
+                          <?php
+                              include 'includes/southAmerica.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingThree">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-decoration-none text-info text-left" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseThree">
+                          North America
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseFive" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                          <div class="card-body text-dark">
+                          <?php
+                              include 'includes/northAmerica.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card">
+                        <div class="card-header" id="headingThree">
+                          <h5 class="mb-0">
+                          <button class="btn btn-link w-100 text-decoration-none text-info text-left" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseThree">
+                          Oceania
+                          </button>
+                          </h5>
+                        </div>
+                        <div id="collapseSix" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                          <div class="card-body text-dark">
+                          <?php
+                              include 'includes/oceania.php';
+                            ?>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  </div>
+                  
+                  
                 </div>
-               
-            </nav>
-        </div>
+              </div>
+            </div>            
+          </div>  
+        </nav>
+      </div>
     </div>
+
+
+
 
     <div class="container-fluid p-3" id="city-temperature">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-6 text-center">
-                    <i class="fas fa-cloud-moon"></i> Current City Temperature
-                </div>
-            </div>
-        </div>    
-    </div>
-    <div class="container-fluid p-3" style="background-color:#0C1F3E">
-        <div class="container">
-            <div class="row">
-                <ul class="list-inline m-auto">
-                    <li class="list-inline-item px-5"><a href="">Today</a></li>
-                    <li class="list-inline-item px-5"><a href="">Hourly</a></li>
-                    <li class="list-inline-item px-5"><a href="">5-day</a></li>
-                    <li class="list-inline-item px-5"><a href="">10-day</a></li>
-                    <li class="list-inline-item px-5"><a href="">Maps</a></li>
-                </ul>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-md-6 col-lg-6 text-center">
+            <i class="fas fa-cloud-moon"></i> Current City Temperature
+          </div>
         </div>
+      </div>
+  	</div>
+
+
+    <div class="container-fluid p-3" style="background-color:#0C1F3E">
+      <div class="container">
+        <div class="row">
+          <ul class="list-inline m-auto">
+            <li class="list-inline-item px-5"><a href="">Today</a></li>
+            <li class="list-inline-item px-5"><a href="">Hourly</a></li>
+            <li class="list-inline-item px-5"><a href="">5-day</a></li>
+            <li class="list-inline-item px-5"><a href="">10-day</a></li>
+            <li class="list-inline-item px-5"><a href="">Maps</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
 
   
-</body>
-
+  </body>
 </html>
